ff4a887af2bc189860467376a49d7eb3c8da30ef
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2023-05-22 20:30:09 +0200

    🐐

--- devops/production/infrastructure/providers.tf
+++ devops/production/infrastructure/providers.tf
@@ -1,5 +1,5 @@
 provider "digitalocean" {
-  token = data.doppler_secrets.commons.map.digitalocean_access_token
+  token = var.digitalocean_api_token
   # spaces_access_id  = data.doppler_secrets.commons.map.digitalocean_spaces_access_id
   # spaces_secret_key = data.doppler_secrets.commons.map.digitalocean_spaces_secret_key
 }
