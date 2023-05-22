da91da764d623ec4ec0e965443c0c2a7de423004
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2023-05-22 20:22:28 +0200

    🐐

--- devops/production/infrastructure/kubernetes.tf
+++ devops/production/infrastructure/kubernetes.tf
@@ -1,53 +1,53 @@
-# data "digitalocean_kubernetes_versions" "kubernetes-version" {
-#   version_prefix = "1.24."
-# }
+data "digitalocean_kubernetes_versions" "kubernetes-version" {
+  version_prefix = "1.24."
+}
 
-# data "digitalocean_sizes" "small" {
-#   filter {
-#     key    = "slug"
-#     values = ["s-1vcpu-2gb"]
-#   }
-# }
+data "digitalocean_sizes" "small" {
+  filter {
+    key    = "slug"
+    values = ["s-1vcpu-2gb"]
+  }
+}
 
-# resource "digitalocean_kubernetes_cluster" "laravel-in-kubernetes" {
-#   name = data.doppler_secrets.app.map.trustup_app_key
-#   region = data.doppler_secrets.commons.map.digitalocean_region
+resource "digitalocean_kubernetes_cluster" "laravel-in-kubernetes" {
+  name = data.doppler_secrets.app.map.trustup_app_key
+  region = data.doppler_secrets.commons.map.digitalocean_region
 
-#   # Latest patched version of DigitalOcean Kubernetes.
-#   # We do not want to update minor or major versions automatically.
-#   version = data.digitalocean_kubernetes_versions.kubernetes-version.latest_version
+  # Latest patched version of DigitalOcean Kubernetes.
+  # We do not want to update minor or major versions automatically.
+  version = data.digitalocean_kubernetes_versions.kubernetes-version.latest_version
 
-#   # We want any Kubernetes Patches to be added to our cluster automatically.
-#   # With the version also set to the latest version, this will be covered from two perspectives
-#   auto_upgrade = true
-#   maintenance_policy {
-#     # Run patch upgrades at 4AM on a Sunday morning.
-#     start_time = "04:00"
-#     day = "sunday"
-#   }
+  # We want any Kubernetes Patches to be added to our cluster automatically.
+  # With the version also set to the latest version, this will be covered from two perspectives
+  auto_upgrade = true
+  maintenance_policy {
+    # Run patch upgrades at 4AM on a Sunday morning.
+    start_time = "04:00"
+    day = "sunday"
+  }
 
-#   node_pool {
-#     name = data.doppler_secrets.app.map.trustup_app_key
-#     size = "${element(data.digitalocean_sizes.small.sizes, 0).slug}"
-#     node_count = 1
-#     # We can autoscale our cluster according to use, and if it gets high,
-#     # We can auto scale to maximum 5 nodes.
-#     auto_scale = false
-#     # min_nodes = 1
-#     # max_nodes = 5
+  node_pool {
+    name = data.doppler_secrets.app.map.trustup_app_key
+    size = "${element(data.digitalocean_sizes.small.sizes, 0).slug}"
+    node_count = 1
+    # We can autoscale our cluster according to use, and if it gets high,
+    # We can auto scale to maximum 5 nodes.
+    auto_scale = false
+    # min_nodes = 1
+    # max_nodes = 5
 
-#     # These labels will be available in the node objects inside of Kubernetes,
-#     # which we can use as taints and tolerations for workloads.
-#     labels = {
-#       pool = "default"
-#       size = "small"
-#     }
-#   }
-# }
+    # These labels will be available in the node objects inside of Kubernetes,
+    # which we can use as taints and tolerations for workloads.
+    labels = {
+      pool = "default"
+      size = "small"
+    }
+  }
+}
 
-# resource "doppler_secret" "digitalocean_kubernetes_cluster_id" {
-#   project = data.doppler_secrets.app.map.trustup_app_key
-#   config = "production-ci" #TODO
-#   name = "DIGITALOCEAN_KUBERNETES_CLUSTER_ID"
-#   value = digitalocean_kubernetes_cluster.laravel-in-kubernetes.id
-# }
+resource "doppler_secret" "digitalocean_kubernetes_cluster_id" {
+  project = data.doppler_secrets.app.map.trustup_app_key
+  config = "production-ci" #TODO
+  name = "DIGITALOCEAN_KUBERNETES_CLUSTER_ID"
+  value = digitalocean_kubernetes_cluster.laravel-in-kubernetes.id
+}
--- devops/production/infrastructure/providers.tf
+++ devops/production/infrastructure/providers.tf
@@ -1,8 +1,8 @@
-# provider "digitalocean" {
-#   token = data.doppler_secrets.commons.map.digitalocean_access_token
-#   # spaces_access_id  = data.doppler_secrets.commons.map.digitalocean_spaces_access_id
-#   # spaces_secret_key = data.doppler_secrets.commons.map.digitalocean_spaces_secret_key
-# }
+provider "digitalocean" {
+  token = data.doppler_secrets.commons.map.digitalocean_access_token
+  # spaces_access_id  = data.doppler_secrets.commons.map.digitalocean_spaces_access_id
+  # spaces_secret_key = data.doppler_secrets.commons.map.digitalocean_spaces_secret_key
+}
 
 provider "doppler" {
   doppler_token = var.doppler_ci_commons_token
