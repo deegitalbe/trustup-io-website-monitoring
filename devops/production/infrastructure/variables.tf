708f8c3b2f84eb386dd7fc6dd34a370039433360
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2023-05-23 19:02:01 +0200

    🐐

--- devops/production/infrastructure/variables.tf
+++ devops/production/infrastructure/variables.tf
@@ -1,14 +1,14 @@
 variable "DOPPLER_CI_COMMONS_TOKEN" {
   type = string
-  default = "dp.st.prd.fpgrv3ebtM2zPQrxouW2CuOcRmzYMKAmLSm2f3sQvgT"
+  # default = "dp.st.prd.fpgrv3ebtM2zPQrxouW2CuOcRmzYMKAmLSm2f3sQvgT"
 }
 
 variable "DOPPLER_CI_APP_TOKEN" {
   type = string
-  default = "dp.st.production-ci.9LDJFyBLzmLxU1O70jIDR8I3YILuyS1WCNdzYubZf8p"
+  # default = "dp.st.production-ci.9LDJFyBLzmLxU1O70jIDR8I3YILuyS1WCNdzYubZf8p"
 }
 
 variable "DOPPLER_CI_USER_TOKEN" {
   type = string
-  default = "dp.pt.mgbINyq0uimPcALne3iKTt3ot9pwKdVP4iIQPTQK1ru"
+  # default = "dp.pt.mgbINyq0uimPcALne3iKTt3ot9pwKdVP4iIQPTQK1ru"
 }
