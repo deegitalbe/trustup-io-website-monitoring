b7952ecf3416b98859dd29c7b52fde9b10b00cf9
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2023-06-12 19:01:57 +0200

    🧪

--- devops/infrastructure/app/traefik_helm_release.tf
+++ devops/infrastructure/app/traefik_helm_release.tf
@@ -1,4 +1,5 @@
 resource "helm_release" "traefik" {
+  depends_on = [ kubectl_manifest.doppler_traefik_secret, kubectl_manifest.doppler, kubernetes_secret.ci_commons_token ]
   namespace = kubernetes_namespace.traefik.metadata[0].name
   create_namespace = true
   name = "traefik"
